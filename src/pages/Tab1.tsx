5beb18cf5ba7d5995112c36ede897c3f3592a3ba
yiyan wang <13171209963@163.com>
2022-03-11 23:59:47 +1100

    Add a new button in Tag2 for the specific request and draw a point on the map to locate Sydney

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,14 +2,32 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewDidEnte
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
-//import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
-import { Cartesian3 ,Ion, Viewer} from 'cesium';
+import { Cartesian3 ,Ion, Viewer, HorizontalOrigin, VerticalOrigin, DistanceDisplayCondition, Color} from 'cesium';
 
 Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMGFjYTVjNC04OTJjLTQ0Y2EtYTExOS1mYzAzOWFmYmM1OWQiLCJpZCI6MjA4OTksInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1Nzg1MzEyNjF9.KyUbfBd_2aCHlvBlrBgdM3c3uDEfYyKoEmWzAHSGSsk';
 
+
+
 const Tab1: React.FC = () => {
+
   useIonViewDidEnter(() => {
     var viewer = new Viewer('cesiumContainer');
+
+    var AustraliaPin = viewer.entities.add({
+      name: 'Australia',
+      position: Cartesian3.fromDegrees(151.194191, -34.035875,1000),
+      point: {
+        color: Color.YELLOW,
+        pixelSize: 10
+      },
+      label: {
+        text: 'Sydney',
+        horizontalOrigin: HorizontalOrigin.CENTER,
+        verticalOrigin: VerticalOrigin.BOTTOM,
+      }
+    })
+    viewer.flyTo(AustraliaPin)
+
   });
 
   return (
