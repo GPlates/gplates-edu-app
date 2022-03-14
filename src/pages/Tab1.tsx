55eac12c09e39673066f496aac03f1ac8ebf9656
matthewmerkas <matthew.merkas@gmail.com>
2022-03-14 13:14:45 +1100

    Migrated to resium. Added point on globe for Madsen Building.

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,23 @@
-import { IonContent, IonPage, useIonViewDidEnter } from '@ionic/react';
+import {IonPage} from '@ionic/react';
+import {Entity, Viewer} from 'resium';
 import './Tab1.css';
-
-//import { Viewer, Entity, PointGraphics, EntityDescription } from 'resium';
-import { Ion, Viewer } from 'cesium';
+import {Cartesian3, Color, Ion} from "cesium";
 
 Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMGFjYTVjNC04OTJjLTQ0Y2EtYTExOS1mYzAzOWFmYmM1OWQiLCJpZCI6MjA4OTksInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1Nzg1MzEyNjF9.KyUbfBd_2aCHlvBlrBgdM3c3uDEfYyKoEmWzAHSGSsk';
 
 const Tab1: React.FC = () => {
-  useIonViewDidEnter(() => {
-      const viewer = new Viewer('cesiumContainer');
-  });
-
-  return (
-    <IonPage>
-
-      <IonContent fullscreen>
-        <div id="cesiumContainer" />
-      </IonContent>
-
-    </IonPage>   
-  );
+    return (
+        <IonPage>
+            <Viewer full>
+                <Entity
+                    name="Madsen Building"
+                    description="The University of Sydney"
+                    point={{ pixelSize: 20, color: Color.FUCHSIA }}
+                    position={Cartesian3.fromDegrees(151.189422, -33.888688, 100)}
+                />
+            </Viewer>
+        </IonPage>
+    );
 };
 
 
